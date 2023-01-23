daaa246c5f69efe8ef454cb13bc73ff4316e887e
kadekwahyu21102006 <117607353+kadekwahyu1925@users.noreply.github.com>
2023-01-23 11:16:41 +0800

    first commit

---
+++ resources/views/code.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.default')
+
+@section('content')
+<section>
+    <div class="conttainer mt-5">
+        <div class="row">
+            <div class="col-lg-8">
+<h3>CODINGAN PROJECT SENSOR DAN TRANDUSER</h3>
+</div>
+          <div class ="col-lg-55">
+          <table class="table table-bordered border-primary-subtle">
+            <tr>
+            <center><img src="img/code1.png" width="1080px" ></center>
+            <center><img src="img/code2.png" width="1080px" ></center>
+            <center><img src="img/code3.png" width="1080px" ></center>
+            <center><img src="img/code4.png" width="1080px" ></center>
+            <center><img src="img/code5.png" width="1080px" ></center>
+            <center><img src="img/code6.png" width="1080px" ></center>
+            <center><img src="img/code7.png" width="1080px" ></center>
+
+            </tr>
+                    
+                    
+</section>
+@endsection
